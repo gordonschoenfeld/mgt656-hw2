fa71d1402bb17912c9bae6d62a936a6ca7f953e4
Gordon Schoenfeld <gordon.schoenfeld@gmail.com>
2015-01-28 05:45:25 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656');
+});
+
+
+app.get('/nickname', function(request, response){
+    response.send('proud partridge');
 });
 
 app.listen(process.env.PORT || 4000);
